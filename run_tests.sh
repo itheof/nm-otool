f58822f51eccd02b241438289bebace0041ca35d
itheof <tvallee@student.42.fr>
2018-01-03 19:23:32 +0100

    testing error output and exit status

--- run_tests.sh
+++ run_tests.sh
@@ -15,7 +15,11 @@ EOF
 }
 
 cleanup() {
-	rm -f "${TEST_PATH}/ref.log" "${TEST_PATH}/test.log"
+	rm -f "${TEST_PATH}"/ref_{out,err}.log "${TEST_PATH}"/test_{out,err}.log
+}
+
+assert () {
+	[ $1 -eq $2 ] || (echo "Return values differ: $1 != $2 !" >&2 ; exit 1)
 }
 
 compare () { # 1: Ref binary 2: Test binary 3+: Options
@@ -23,10 +27,19 @@ compare () { # 1: Ref binary 2: Test binary 3+: Options
 	for file in $files;
 	do
 		log Running \`"$1" $3 "$file"\`
-		"$1" $3 "$file" > "${TEST_PATH}/ref.log"
+		"$1" $3 "$file" \
+			> "${TEST_PATH}/ref_out.log" \
+			2> "${TEST_PATH}/ref_err.log" \
+			&& ret_ref=$? || ret_ref=$?
+
 		log Running \`"$2" $3 "$file"\`
-		"$2" $3 "$file" > "${TEST_PATH}/test.log"
-		diff "${TEST_PATH}/ref.log" "${TEST_PATH}/test.log"
+		"$2" $3 "$file" \
+			> "${TEST_PATH}/test_out.log" \
+			2> "${TEST_PATH}/test_err.log" \
+			&& ret_test=$? || ret_test=$?
+		diff "${TEST_PATH}"/{ref,test}_out.log
+		assert $ret_ref $ret_test
+		cleanup
 	done
 }
 
